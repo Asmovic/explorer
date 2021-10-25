233d2444f26aeb873379d2320a01499d8c182f11
Asmovic <asogba007@gmail.com>
2021-10-25 18:39:42 +0100

    retry stripe integration

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -46,11 +46,15 @@ exports.getCheckoutSession = catchAsync(async (req,res,next)=>{
 } */
 
 const createBookingCheckout = async session =>{
+try{
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
     const price = session.display_items[0].amount / 100;
     console.log('Booking....'. tour, user, price);
     await Booking.create({ tour, user, price });
+}catch(err){
+    return next(new AppError('Error creating Booking', 400))
+}
 }
 
 exports.webhookCheckout = (req, res, next)=>{
