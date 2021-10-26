7fc34eda99c5effd612fb5424021a90391babb5e
Asmovic <asogba007@gmail.com>
2021-10-26 17:41:41 +0100

    Send alert on booking completion

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -81,8 +81,7 @@ exports.updateUserAccount = async (req,res)=>{
 exports.alerts = (req,res,next)=>{
     const { alert } = req.query;
     if (alert === 'booking'){
-        req.locals.alert = "your booking was successful! " +
-        "Please ckeck your email for a comfirmation. If your booking doesn't show up here immediately, please come back later.";
+        res.locals.alert = "your booking was successful! Please ckeck your email for a comfirmation. If your booking doesn't show up here immediately, please come back later.";
     }
     next();
 }
